a86e7532947476e528042511eb7cbc799cda2cc1
kimihiro_n <kimihiro.nisimura@gmail.com>
2017-07-11 21:57:42 +0900

    Install admob

--- app/src/main/kotlin/com/appspot/pistatium/unluckymeter/MainActivity.kt
+++ app/src/main/kotlin/com/appspot/pistatium/unluckymeter/MainActivity.kt
@@ -2,11 +2,19 @@ package com.appspot.pistatium.unluckymeter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class MainActivity : AppCompatActivity() {
+
+    private var adView: AdView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adView = findViewById(R.id.adView) as AdView?
+        val adRequest = AdRequest.Builder().build()
+        adView?.loadAd(adRequest)
     }
 }
